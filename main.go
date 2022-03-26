e770fd1f41729e39275bba2e3e171c4558049dac
Kshitij Raj <kr91323@gmail.com>
2022-03-27 02:49:13 +0530

    main

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main() { //giving entry point
 	var confname string = "Go conference"
 	const conftickets int = 50
 	var remainedtick uint = 50
+	var bookings [50]string //making an array size of array_data type
 
 	//fmt.Printf("Total %T , reamining %T \n", conftickets, remainedtick) //%T is palceholder for the variable and prints the type of variable
 
@@ -17,7 +18,7 @@ func main() { //giving entry point
 	var usrname string
 	var lstname string
 	var email string
-	var usrticket int
+	var usrticket uint
 
 	//asking for input
 	fmt.Println("Enter your name")
@@ -35,7 +36,18 @@ func main() { //giving entry point
 
 	//usrname = "Tom"
 	//usrticket = 2
+
+	//logic
+	remainedtick = remainedtick - usrticket //always keep same data type
+	bookings[0] = usrname + " " + lstname   //storing values to array
+
+	fmt.Printf("Array for booking : %v \n", bookings)
+	fmt.Printf("The first value : %v \n", bookings)
+	fmt.Printf("Array type:%T \n", bookings)        //type of an array
+	fmt.Printf("Array length:%v \n", len(bookings)) //length of an array
+
 	fmt.Println(usrname)
 	fmt.Printf("User %v %v booked %v tickets. Will be reciving mail soon at %v \n", usrname, lstname, usrticket, email) //printf formats the message and require a %v for refering the variable
+	fmt.Printf(" Remaining tickets : %v", remainedtick)
 
 }
